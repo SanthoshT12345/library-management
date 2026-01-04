e543e327767fe09692cbf00dd36ed76b128ef125
SanthoshT12345 <santhoshthirumalai70@gmail.com>
2026-01-04 18:33:35 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Library Management API is running 🚀");
+});
+
 app.use("/api/books", bookRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

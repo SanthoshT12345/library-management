9ca481180d05db1a6c743ab986cd2b2e09a33b50
SanthoshT12345 <santhoshthirumalai70@gmail.com>
2026-01-04 17:51:16 +0530

    Fix frontend

--- frontend/src/services/bookService.js
+++ frontend/src/services/bookService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/books";
+const BASE_URL = import.meta.env.VITE_API_URL + "/api/books";
+
+
 
 export const getAllBooks = async () => {
   const res = await axios.get(BASE_URL);

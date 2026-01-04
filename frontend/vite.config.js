609c6846f8c9139f8cca4885c32efa52a60fe039
SanthoshT12345 <santhoshthirumalai70@gmail.com>
2026-01-04 19:21:30 +0530

    Add GitHub Actions deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/library-management/',   // 👈 EXACT repo name
+  base: '/library-management/', // 👈 EXACT GitHub repo name
 })

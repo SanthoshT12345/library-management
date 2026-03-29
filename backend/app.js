55efcef7acc94a6a7d56fbcc0b8e3c5a2b34625d
SanthoshT12345 <santhoshthirumalai70@gmail.com>
2026-03-29 17:37:55 +0530

    Update API baseURL to include '/books' endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://library-backend-bn1m.onrender.com/api"
+  baseURL: "https://library-backend-bn1m.onrender.com/api/books"
 });
+
 
 export default API;
